2646eea430aa9696004248a72aee4e364f5871f5
filki <83698@student.ue.poznan.pl>
2024-12-02 19:42:17 +0100

    various templates update

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
-from flask import render_template
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_input = request.form['user_input']
+    # Możesz dodać dowolne operacje na danych tutaj
+    return render_template('results.html', data=user_input)
 
 if __name__ == '__main__':
     app.run(debug=True)
