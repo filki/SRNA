24a5031d9f7865e15f807896891c3d289ecc7b7b
filki <83698@student.ue.poznan.pl>
2024-12-02 19:30:25 +0100

    added template usage

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+from flask import render_template
 
 @app.route('/')
 def home():
-    return 'Analza recenzji Steam'
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
